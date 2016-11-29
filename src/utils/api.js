5fc99620aefa665230a485c6bf2e6f391572ba1c
RasCarlito <rascarlito@gmail.com>
2016-11-29 12:23:54 +0100

    Create marvel api util

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,1 +1,37 @@
-const http = require('http');
+const $ = require('jquery')
+const urlComposer = require('url-composer')
+const Promise = require('bluebird')
+
+const config = {
+  host: 'http://gateway.marvel.com/v1/public',
+  key: '7fa20a1ca0864298079862904d027f87',
+
+  api: {
+    comics: {
+      path: '/comics'
+    }
+  }
+}
+
+module.exports = {
+  get (name, params) {
+    const url = urlComposer.build({
+      host: config.host,
+      path: config.api[name].path,
+      query: { apikey: config.key },
+      params
+    })
+
+    return new Promise((resolve, reject) => {
+      $.ajax({
+        url,
+        success: (res) => {
+          resolve(res.data.results)
+        },
+        error: (xhr, res, err) => {
+          reject(err)
+        }
+      })
+    })
+  }
+}
